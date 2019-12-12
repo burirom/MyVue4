f8f25851780ffe3f146d0bca4c356a99feb8bfd0
a50610 <a50610@a50610-no-iMac.local>
2019-12-12 11:07:04 +0900

    Fourth

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-
 new Vue({
-	el: '#app',
-	data: {
-		show: true
-	}
+    el: '#app',
+    data: {
+        list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    methods: {
+        doShuffle: function () {
+            this.list = _.shuffle(this.list)
+        },
+        doAdd: function () {
+            var newNumber = Math.max.apply(null, this.list) + 1
+            var index = Math.floor(Math.random() * (this.list.length + 1))
+            this.list.splice(index, 0, newNumber)
+        },
+        doRemove: function (index) {
+            this.list.splice(index, 1)
+        },
+        beforeLeave: function (el) {
+            var style = window.getComputedStyle(el)
+            el.style.left = el.offsetLeft - parseFloat(style.marginLeft, 10) + 'px'
+            el.style.top = el.offsetTop - parseFloat(style.marginTop, 10) + 'px'
+            el.style.backgroundColor= "#fff000"
+        }
+    }
 })
